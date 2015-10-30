f00256be46e773425241d1bead4d552cd71cc4cc
Marc Hammerton <marc@hammerton.de>
2015-10-30 13:48:48 +0100

    add JSON REST service for teams

---
+++ routes/api/teams.js
@@ -0,0 +1,44 @@
+/**
+ * routes/api/teams.js
+ */
+
+var models = require('../../models');
+var express = require('express');
+var router = express.Router();
+
+/* GET list of teams */
+router.get('/', function(req, res) {
+	models.Team.findAll().then(function(err, teams) {
+		if (err) {
+			res.send(err);
+		}
+		
+		res.json(teams);
+	});
+});
+
+/* POST add a team */
+router.post('/', function(req, res) {
+	models.Team.create(req.body, {fields: ["name", "team", "player1", "player2"]})
+		.then(function(team) {
+			res.json(team.id);
+		});
+});
+
+/* GET get a team */
+router.post('/:id', function(req, res) {
+	models.Team.findByPrimary(req.params.id)
+		.then(function(team) {
+			res.json(team);
+		});
+});
+
+/* PUT update a team */
+router.put('/:id', function(req, res) {
+	models.Team.update(req.body, {where: {id: req.params.id}, fields: ["name", "team", "player1", "player2"]})
+		.then(function() {
+			res.json(req.params.id);
+		});
+});
+
+module.exports = router;
