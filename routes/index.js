b2c50133b73a9d11c40a110baf60b841d990c507
Marc Hammerton <marc@hammerton.de>
2015-10-30 17:41:14 +0100

    add simple index template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	models.Team.findAll().then(function(teams) {
+		models.MatchDay.findAll().then(function(matchDays) {
+			models.Match.findAll({ 
+				include: [ 
+				          { model: models.Team, as: "team1" },
+				          { model: models.Team, as: "team2" }
+				]})
+				.then(function(matches) {
+					matchDays.forEach(function(matchDay) {
+					matchDay.matches = matches.filter(function(match) {
+						return match.matchDayNumber === matchDay.number;
+					});
+				});
+				
+				res.render('index', {teams: teams, matchDays: matchDays});
+			});
+		});
+	});
 });
 
 module.exports = router;
