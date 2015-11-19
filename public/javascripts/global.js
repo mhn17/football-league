6b1d88a4902a56906ec414a3db19ff27abee54e3
Marc Hammerton <marc@hammerton.de>
2015-11-19 12:40:00 +0100

    add select box for selecting the match day

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,7 +1,14 @@
 // DOM Ready ===================================================================
 $(document).ready(function() {
-	
+	initShowMatchDay();
 });
 
 // Functions ===================================================================
+function initShowMatchDay() {
+	$('#showMatchDay').on('change', function () {
+		var selected = $(this).find("option:selected").val();
+		$('.matchDayWrapper').removeClass('hidden').hide();
+		$('.matchDayWrapper.' + selected).show();
+	});
 
+};
