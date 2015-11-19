7bb4a9470276e45a9eb3f91558b311bf51201d3e
Marc Hammerton <marc@hammerton.de>
2015-11-19 11:14:14 +0100

    show table on page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,28 +2,48 @@
  * app/routes/index.js
  */
 
-var models  = require('../models');
+var async = require('async');
+var models  = require('./../models');
 var express = require('express');
 var router = express.Router();
+var TableService = require('./../services/tableService')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	models.Team.findAll().then(function(teams) {
-		models.MatchDay.findAll().then(function(matchDays) {
-			models.Match.findAll({ 
-				include: [ 
-				          { model: models.Team, as: "team1" },
-				          { model: models.Team, as: "team2" }
+	var parameterStack = {};
+	
+	parameterStack.matchData = function(callback) {
+		models.MatchDay.findAll().then(function (matchDays) {
+			models.Match.findAll({
+				include: [
+					{model: models.Team, as: "team1"},
+					{model: models.Team, as: "team2"}
 				]})
-				.then(function(matches) {
-					matchDays.forEach(function(matchDay) {
-					matchDay.matches = matches.filter(function(match) {
-						return match.matchDayNumber === matchDay.number;
+				.then(function (matches) {
+					matchDays.forEach(function (matchDay) {
+						matchDay.matches = matches.filter(function (match) {
+							return match.matchDayNumber === matchDay.number;
+						});
 					});
+					
+					callback(null, {matchDays: matchDays, matches: matches});
 				});
-				
-				res.render('index', {teams: teams, matchDays: matchDays});
-			});
+		});
+	};
+	
+	parameterStack.teams = function(callback) {
+		models.Team.findAll().then(function (teams) {
+			callback(null, teams);
+		});		
+	};
+	
+	async.parallel(parameterStack, function(err, results) {
+		var tableService = new TableService(results.matchData.matches, results.teams);
+		var table =  tableService.getCurrentTable();
+		res.render('index', {
+			table: tableService.getCurrentTable(),
+			teams: results.teams, 
+			matchDays: results.matchData.matchDays
 		});
 	});
 });
