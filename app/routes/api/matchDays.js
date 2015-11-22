8c3605e96ca7d36e53b078029271f4a3cb19f88c
Marc Hammerton <marc@hammerton.de>
2015-11-22 22:13:55 +0100

    add match day form

--- app/routes/api/matchDays.js
+++ app/routes/api/matchDays.js
@@ -13,11 +13,14 @@ router.get('/', function(req, res) {
 	});
 });
 
-/* POST add a match day */
+/* POST add a match day */process.php
 router.post('/', function(req, res) {
 	models.MatchDay.create(req.body, {fields: ["number", "dateFrom", "dateTo"]})
 		.then(function(matchDay) {
 			res.json(matchDay.number);
+		})
+		.catch(function (error) {
+			res.status(400).json(error);
 		});
 });
 
@@ -26,7 +29,10 @@ router.get('/:number', function(req, res) {
 	models.MatchDay.findByPrimary(req.params.number)
 		.then(function(matchDay) {
 			res.json(matchDay);
-		});
+		})
+		.catch(function (error) {
+			res.status(400).json(error);
+		});;
 });
 
 /* PUT update a match day */
@@ -34,7 +40,10 @@ router.put('/:number', function(req, res) {
 	models.MatchDay.update(req.body, {where: {number: req.params.number}, fields: ["number", "dateFrom", "dateTo"]})
 		.then(function() {
 			res.json(req.params.number);
-		});
+		})
+		.catch(function (error) {
+			res.status(400).json(error);
+		});;
 });
 
 module.exports = router;
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,17 +1,17 @@
 // DOM Ready ===================================================================
 $(document).ready(function() {
 	initShowMatchDay();
+	initAddMatchDayModal();
 });
 
 // Functions ===================================================================
+// match day
 function initShowMatchDay() {
 	// remove hidden class and hide using jquery
 	$('.matchDayWrapper').removeClass('hidden').hide();
 	
 	// select last match day
-	var value = $('#showMatchDay option:last-child').val();
-	$('#showMatchDay option:last-child').attr('selected', 'selected');	
-	$('.matchDayWrapper.' + value).show();
+	showLastMatchDay();
 	
 	// add onChange method
 	$('#showMatchDay').on('change', function () {
@@ -19,5 +19,55 @@ function initShowMatchDay() {
 		$('.matchDayWrapper').hide();
 		$('.matchDayWrapper.' + selected).show();
 	});
-
 };
+
+function showLastMatchDay() {
+	var value = $('#showMatchDay option:last-child').val();
+	$('#showMatchDay option:last-child').attr('selected', 'selected');
+	$('.matchDayWrapper.' + value).show();
+};
+
+function updateShowMatchDay() {
+	$.ajax({
+		type: 'GET',
+		url: 'api/match-days',
+		success: function (data) {
+			var $showMatchDaySelect = $("#showMatchDay");
+			$showMatchDaySelect.empty(); 
+			$.each(data, function (index, matchDay) {
+				$showMatchDaySelect.append($("<option></option>")
+					.attr("value", matchDay.number).text(matchDay.number + 
+						" (" + matchDay.dateFrom + " - " + matchDay.dateTo + ")"));
+			});
+			showLastMatchDay();
+		},
+		error: function () {
+			$("#messageWrapper").html('<div class="alert alert-danger" role="alert">' +
+				'<strong>Es ist ein Fehler aufgetreten. Die Spieltage wurden nicht aktualisiert!</strong>' +
+				'</div>');
+		}
+	});
+}
+
+function initAddMatchDayModal() {
+	$('#submitAddMatchDay').on('click', function() {
+		$.ajax({
+			type: 'POST',
+			url: 'api/match-days',
+			data: $('form#addMatchDayForm').serialize(),
+			success: function (msg) {
+				updateShowMatchDay();
+				$("#addMatchDayModal").modal('hide');
+				$("#messageWrapper").html('<div class="alert alert-success" role="alert">' +
+					'<strong>Spieltag wurde gespeicher!</strong>' +
+					'</div>');
+			},
+			error: function () {
+				$("#addMatchDayModal").modal('hide');
+				$("#messageWrapper").html('<div class="alert alert-danger" role="alert">' +
+					'<strong>Es ist ein Fehler aufgetreten. Der Spieltag wurde nicht gespeichert!</strong>' +
+					'</div>');
+			}
+		});
+	});
+}
